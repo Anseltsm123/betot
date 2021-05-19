3ebb324c242872521b6740399408a911a2476cd1
Anseltsm123 <80182333+Anseltsm123@users.noreply.github.com>
2021-05-19 15:51:15 +0700

    Update iklan.js

--- iklan.js
+++ iklan.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const channel = "843444578803384363";
+const token = require("./config.json")
 
 const iklan = require("./iklan.json")
       setInterval (() => {
@@ -22,4 +23,4 @@ const embed = new Discord.RichEmbed()
 }, 60000);
 console.log('iklan siap');
 
-client.login('ODQzMzQ1NTcxNzQxOTU4MTg0.YKCgxA.WSmUxA4vW2UvvomsJ_R3bWGvm4Q');
+client.login(token);
